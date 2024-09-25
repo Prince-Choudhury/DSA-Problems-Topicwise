ef68303a3a86092a5e061731e2a7bdbbc8e6ddc8
Prince Choudhury <choudhuryprince19@gmail.com>
2024-09-25 17:31:46 +0530

    updated code

---
+++ 9_Linked List/LL_Class4_Code.cpp
@@ -0,0 +1,98 @@
+//1. Linked List Cycle(Loop)
+
+//Using map
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        map<ListNode*, bool> table;
+        
+        ListNode* temp = head;
+
+        //Inside the loop, we check if the current node temp is present 
+        //in the map table. If it's not present (i.e., table[temp] is false), 
+        //we mark it as visited by setting table[temp] to true.
+
+        while(temp != NULL){
+            if(table[temp] == false){
+                table[temp] = true;
+            }
+
+            else{
+                //cycle present
+                return true;
+            }
+
+            temp = temp->next;
+        }
+
+        //cycle not present 
+        return false;
+    }
+};
+
+//Method - 2 : Slow and fast method
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow = slow->next;
+            }
+
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};    
+
+
+
+//2. Starting point of loop (Linked List Cycle II)
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        //check cycle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL){
+            fast = fast -> next;
+            if(fast != NULL){
+                fast = fast -> next;
+                slow = slow -> next;
+            }
+
+            if(fast == slow){
+                break;
+            }
+        }
+
+        if(fast == NULL){
+            //No cycle present
+            return NULL;
+        }
+
+        //Yaha pahucha matlab slow and fast meet kar chuke h
+        slow = head;
+
+        // Slow and fast ek - ek step
+        while(fast != slow){
+            slow = slow -> next;
+            fast = fast -> next;
+            
+        }
+        // return starting point
+        return slow;
+        
+    }
+};
