32394161c313e67842b66d070ee279bb40647b31
Prince Choudhury <choudhuryprince19@gmail.com>
2024-09-30 08:54:55 +0530

    updated the code

--- 10_Stacks/stack class 1 codes/c4.cpp
+++ 10_Stacks/stack class 1 codes/c4.cpp
@@ -1,6 +1,10 @@
+// Recursive Approach
+
 #include <iostream>
 #include<stack>
 using namespace std;
+
+/*
 
 void solve(stack<int> &st, int& pos, int &ans) {
   //base case
@@ -51,10 +55,57 @@ int main() {
   st.push(50);
   st.push(60);
 
-  cout << "before size" << st.size() << endl; 
+  cout << "before size : " << st.size() << endl; 
   int mid = getMiddleElement(st);
   cout << "Middle element: " << mid << endl;
 
-  cout << "after size" << st.size() << endl; 
+  cout << "after size : " << st.size() << endl; 
   return 0;
+}
+
+*/
+// Iterative Approach
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int findMiddle(stack<int>& s) {
+    int n = s.size();
+    int midIdx = n / 2;
+    
+    stack<int> tempStack;
+    // Pop elements until we reach the middle
+    for (int i = 0; i < midIdx; i++) {
+        tempStack.push(s.top());
+        s.pop();
+    }
+    
+    // The middle element
+    int middle = s.top();
+    s.pop();
+    
+    // Push the elements back into the original stack
+    while (!tempStack.empty()) {
+        s.push(tempStack.top());
+        tempStack.pop();
+    }
+    
+    return middle;
+}
+
+int main() {
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    
+    
+    cout << "Middle element: " << findMiddle(s) << endl; 
+    cout<< "Size after deletion : "<<s.size()<<endl; 
+    
+    return 0;
 }
